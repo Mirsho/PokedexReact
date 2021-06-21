0073278782196497f8e2586679d26cdaa375f467
Adrián Perera <mirsho@protonmail.com>
2021-06-21 22:02:17 +0100

    Removed unused import, 3rd deploy try

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,10 +1,8 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
